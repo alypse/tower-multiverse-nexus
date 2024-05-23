1ea41acf6aecdb4f7028a6f8476ddeeee05d50a1
alypse <44419772+alypse@users.noreply.github.com>
2024-05-23 14:50:47 +0100

    add babel dev dependency

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,9 @@
-// Maths
+// Maths //
 export const sum = (a, b) => b ? sum(a ^ b, (a & b) << 1) : a;
 
 export const average = array => array.reduce((a, b) => a + b) / array.length;
 
-// Strings
+// Strings //
 export const capitalize = string => (!string?.length ? '' : string.charAt(0).toUpperCase() + string.substring(1).toLowerCase());
 
 export const capitalizeAll = string => (!string?.length ? '' : string.split(' ').map(capitalize).join(' '));
