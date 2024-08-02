f689c9db8f359e7747c17644858654e02651e240
alypse <44419772+alypse@users.noreply.github.com>
2024-08-02 19:53:13 +0100

    Add Vite

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,6 @@
 import './Main.scss';
 import '../variables.scss';
-import { useInputState } from '../utils/hooks';
+import { useInputState, useIntegerState } from '../utils/hooks';
 import { Calculator } from '../components/Calculator';
 import { PermaCalculator } from '../components/PermaCalculator';
 import React, { useState } from 'react';
@@ -14,24 +14,24 @@ const dwCooldowns = DEATH_WAVE.upgrades.Cooldown.values;
 const bhCooldowns = BLACK_HOLE.upgrades.Cooldown.values;
 const mnEffects = Object.values(MULTIVERSE_NEXUS_EFFECT);
 
-export const VIEWS = {
+const VIEWS = {
   CALCULATOR: 'MN Calculator',
   PERMA_CALCULATOR: 'Perma Calculator',
 };
 
-const Main = () => {
+export const Main = () => {
   const [view, setView] = useInputState(VIEWS.CALCULATOR, 'view');
 
-  const [mnEffect, setMnEffect] = useState(mnEffects[mnEffects.length - 1]);
-  const [gtCooldown, setGtCooldown] = useState(gtCooldowns[gtCooldowns.length - 1].value);
+  const [mnEffect, setMnEffect] = useIntegerState(mnEffects[mnEffects.length - 1], 'calcMvnEffect', -10, 20);
+  const [gtCooldown, setGtCooldown] = useIntegerState(gtCooldowns[gtCooldowns.length - 1].value, 'calcGtCooldown', 100, 300);
   const [gtEnabled, setGtEnabled] = useCheckboxState(true, 'gt');
-  const [dwCooldown, setDwCooldown] = useState(dwCooldowns[dwCooldowns.length - 1].value);
+  const [dwCooldown, setDwCooldown] = useIntegerState(dwCooldowns[dwCooldowns.length - 1].value, 'calcDwCooldown', 300, 100);
   const [dwEnabled, setDwEnabled] = useCheckboxState(true, 'dw');
-  const [bhCooldown, setBhCooldown] = useState(bhCooldowns[bhCooldowns.length - 1].value);
+  const [bhCooldown, setBhCooldown] = useIntegerState(bhCooldowns[bhCooldowns.length - 1].value, 'calcBhCooldown', 50, 200);
   const [bhEnabled, setBhEnabled] = useCheckboxState(true, 'bh');
-  const [gtCooldownSubstat, setGtCooldownSubstat] = useState(0);
-  const [dwCooldownSubstat, setDwCooldownSubstat] = useState(0);
-  const [bhCooldownSubstat, setBhCooldownSubstat] = useState(0);
+  const [gtCooldownSubstat, setGtCooldownSubstat] = useIntegerState(0, 'calcGtCooldownSubstat', 0, 12);
+  const [dwCooldownSubstat, setDwCooldownSubstat] = useIntegerState(0, 'calcDwCooldownSubstat', 0, 13);
+  const [bhCooldownSubstat, setBhCooldownSubstat] = useIntegerState(0, 'setBhCooldownSubstat', 0, 4);
 
 
   const gtCooldownValues = gtCooldowns.map((levels, index) => {
@@ -44,7 +44,8 @@ const Main = () => {
     return levels.value;
   });
 
-  const cds = [];
+
+  const cds: number[] = [];
   if (gtEnabled) cds.push(gtCooldown - gtCooldownSubstat);
   if (dwEnabled) cds.push(dwCooldown - dwCooldownSubstat);
   if (bhEnabled) cds.push(bhCooldown - bhCooldownSubstat);
@@ -54,7 +55,7 @@ const Main = () => {
   let averageCooldownwithMN = 0;
   if (cds.length > 0) {
     averageCooldown = avg(cds);
-    averageCooldownwithMN = sum([averageCooldown, parseInt(mnEffect)]);
+    averageCooldownwithMN = sum([averageCooldown, mnEffect]);
   }
 
   return (
@@ -79,7 +80,7 @@ const Main = () => {
             <div className='control'>
               <label>
                 MN Effect
-                <select value={mnEffect} onChange={e => setMnEffect(e.target.value)}>
+                <select value={mnEffect} onChange={setMnEffect}>
                   {Object.entries(MULTIVERSE_NEXUS_EFFECT).map(([key, value]) => (
                     <option key={key} value={value}>
                       {key}
@@ -94,7 +95,7 @@ const Main = () => {
           <div className='control'>
             <label>
               GT CD
-              <select value={gtCooldown} onChange={e => setGtCooldown(e.target.value)}>
+              <select value={gtCooldown} onChange={setGtCooldown}>
                 {gtCooldownValues.map((value, index) => (
                   <option key={index} value={value}>
                     {value}
@@ -107,7 +108,7 @@ const Main = () => {
           <div className='control'>
             <label>
               GT CD Stat
-              <select value={gtCooldownSubstat} onChange={e => setGtCooldownSubstat(e.target.value)}>
+              <select value={gtCooldownSubstat} onChange={setGtCooldownSubstat}>
                 {Object.entries(GOLDEN_TOWER_SUBSTATS_COOLDOWN).map(([key, value]) => (
                   <option key={key} value={value}>
                     {key}
@@ -122,7 +123,7 @@ const Main = () => {
             <div className='control'>
               <label>
                 DW CD
-                <select value={dwCooldown} onChange={e => setDwCooldown(e.target.value)}>
+                <select value={dwCooldown} onChange={setDwCooldown}>
                   {dwCooldownValues.map((value, index) => (
                     <option key={index} value={value}>
                       {value}
@@ -135,7 +136,7 @@ const Main = () => {
             <div className='control'>
               <label>
                 DW CD Stat
-                <select value={dwCooldownSubstat} onChange={e => setDwCooldownSubstat(e.target.value)}>
+                <select value={dwCooldownSubstat} onChange={setDwCooldownSubstat}>
                   {Object.entries(DEATH_WAVE_SUBSTATS_COOLDOWN).map(([key, value]) => (
                     <option key={key} value={value}>
                       {key}
@@ -150,7 +151,7 @@ const Main = () => {
           <div className='control'>
             <label>
               BH CD
-              <select value={bhCooldown} onChange={e => setBhCooldown(e.target.value)}>
+              <select value={bhCooldown} onChange={setBhCooldown}>
                 {bhCooldownValues.map((value, index) => (
                   <option key={index} value={value}>
                     {value}
@@ -163,7 +164,7 @@ const Main = () => {
           <div className='control'>
             <label>
               BH CD Stat
-              <select value={bhCooldownSubstat} onChange={e => setBhCooldownSubstat(e.target.value)}>
+              <select value={bhCooldownSubstat} onChange={setBhCooldownSubstat}>
                 {Object.entries(BLACK_HOLE_SUBSTATS_COOLDOWN).map(([key, value]) => (
                   <option key={key} value={value}>
                     {key}
