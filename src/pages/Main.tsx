b37dace16434bec6446710e127a9187cbeb8c17f
Alypse <44419772+alypse@users.noreply.github.com>
2024-11-06 12:22:32 +0000

    Test commit

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -27,7 +27,7 @@ const VIEWS = {
 export const Main = () => {
   const [view, setView] = useInputState(VIEWS.MVN_CALCULATOR, 'view');
 
-  const [MultiverseNexusEffect, setMultiverseNexusEffect] = useIntegerState(mnEffects[mnEffects.length - 1], 'calcMvnEffect', -10, 20);
+  const [MultiverseNexusEffect, setMultiverseNexusEffect] = useIntegerState(mnEffects[mnEffects.length - 1], 'calcMnEffect', -10, 20);
   const [MultiverseNexusEnabled, setMultiverseNexusEnabled] = useCheckboxState(true, 'calcMvnEnabled');
   const [gtCooldown, setGtCooldown] = useIntegerState(defaultCooldowns.defaultGoldenTowerCD, 'calcGtCooldown', 100, 300);
   const [gtEnabled, setGtEnabled] = useCheckboxState(true, 'calcGtEnabled');
